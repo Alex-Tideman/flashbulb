ef38bd1a9b2a2b3a962ce414183c1d29363fee2a
Alex Tideman <alex.tideman@gmail.com>
2015-09-10 15:49:00 -0600

    Update home picture, color scheme

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,12 +6,13 @@ class DashboardController < ApplicationController
   end
 
   def feed
-    @posts = current_client.user_media_feed(777)
+    @posts = current_client.user_media_feed
   end
 
   def search
     tags = current_client.tag_search(params[:search])
     if tags[0].nil?
+      flash.now[:notice] = "No search results match!"
       @posts = current_client.user_recent_media
     else
       @posts = current_client.tag_recent_media(tags[0].name)
